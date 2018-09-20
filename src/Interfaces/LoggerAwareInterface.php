eb1f82d9f07a6470555ce61158eb91c2a8cbbbbb
Alex Bacart <13940752+alex-bacart@users.noreply.github.com>
2018-09-20 19:17:24 +0300

    LoggerAware improvements

--- src/Interfaces/LoggerAwareInterface.php
+++ src/Interfaces/LoggerAwareInterface.php
@@ -3,7 +3,6 @@
 namespace Bacart\SymfonyCommon\Interfaces;
 
 use Psr\Log\LoggerInterface;
-use Psr\Log\LogLevel;
 
 interface LoggerAwareInterface extends LoggerInterface
 {
@@ -11,15 +10,4 @@ interface LoggerAwareInterface extends LoggerInterface
      * @param LoggerInterface|null $logger
      */
     public function setLogger(LoggerInterface $logger = null): void;
-
-    /**
-     * @param \Throwable $exception
-     * @param array      $variables
-     * @param string     $level
-     */
-    public function logException(
-        \Throwable $exception,
-        array $variables = [],
-        string $level = LogLevel::ERROR
-    ): void;
 }
