155ade8790b44ce86b6fa3aa265c0b9a1b2d5e75
Alex Bacart <13940752+alex-bacart@users.noreply.github.com>
2019-04-30 14:26:28 +0300

    Code style fixes

--- src/Command/LockableCommandInterface.php
+++ src/Command/LockableCommandInterface.php
@@ -16,10 +16,10 @@ namespace Bacart\SymfonyCommon\Command;
 interface LockableCommandInterface
 {
     public const LOCKABLE_COMMAND_REFRESH_EVENT_NAME = 'lockable_command_refresh_event';
-    public const DISABLE_LOCK_COMMAND_OPTION_NAME = 'disable-lock';
+    public const DISABLE_LOCK_COMMAND_OPTION_NAME    = 'disable-lock';
 
     public const COMMAND_IS_LOCKED_ERROR_CODE = 100;
-    public const DEFAULT_TTL = 60;
+    public const DEFAULT_TTL                  = 60;
 
     /**
      * @return string
