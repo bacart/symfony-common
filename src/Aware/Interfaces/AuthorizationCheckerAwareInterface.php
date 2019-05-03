96c123fdf381ca5ee19a1d16ee3deec9b29d97af
Alex Bacart <13940752+alex-bacart@users.noreply.github.com>
2019-05-04 02:49:30 +0300

    Added AuthorizationChecker aware

---
+++ src/Aware/Interfaces/AuthorizationCheckerAwareInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Bacart package.
+ *
+ * (c) Alex Bacart <alex@bacart.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bacart\SymfonyCommon\Aware\Interfaces;
+
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+interface AuthorizationCheckerAwareInterface
+{
+    /**
+     * @param AuthorizationCheckerInterface $authorizationChecker
+     */
+    public function setAuthorizationChecker(
+        AuthorizationCheckerInterface $authorizationChecker
+    ): void;
+}
---
+++ src/Aware/Traits/AuthorizationCheckerAwareTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Bacart package.
+ *
+ * (c) Alex Bacart <alex@bacart.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bacart\SymfonyCommon\Aware\Traits;
+
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+trait AuthorizationCheckerAwareTrait
+{
+    /** @var AuthorizationCheckerInterface */
+    protected $authorizationChecker;
+
+    /**
+     * @required
+     *
+     * @param AuthorizationCheckerInterface $authorizationChecker
+     */
+    public function setAuthorizationChecker(
+        AuthorizationCheckerInterface $authorizationChecker
+    ): void {
+        $this->authorizationChecker = $authorizationChecker;
+    }
+}
