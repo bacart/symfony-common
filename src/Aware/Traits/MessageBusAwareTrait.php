4789ea0e00cb6914e51dd85ee619043209f600f7
Alex Bacart <13940752+alex-bacart@users.noreply.github.com>
2019-10-12 22:14:15 +0300

    Added MessageBusAware

---
+++ src/Aware/Traits/MessageBusAwareTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Bacart package.
+ *
+ * (c) Alex Bacart <alex@bacart.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bacart\SymfonyCommon\Aware\Traits;
+
+use Symfony\Component\Messenger\MessageBusInterface;
+
+trait MessageBusAwareTrait
+{
+    /** @var MessageBusInterface */
+    protected $bus;
+
+    /**
+     * @required
+     *
+     * @param MessageBusInterface $bus
+     */
+    public function setBus(MessageBusInterface $bus): void
+    {
+        $this->bus = $bus;
+    }
+}
