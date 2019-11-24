e605e5ece17c6b0cedc16a36ff790d02fcbf52c2
Alex Bacart <13940752+alex-bacart@users.noreply.github.com>
2019-11-24 20:00:00 +0300

    Added new awares

---
+++ src/Aware/Traits/CsrfTokenManagerAwareTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Bacart package.
+ *
+ * (c) Alex Bacart <alex@bacart.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bacart\SymfonyCommon\Aware\Traits;
+
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+
+trait CsrfTokenManagerAwareTrait
+{
+    /** @var CsrfTokenManagerInterface */
+    protected $csrfTokenManager;
+
+    /**
+     * @required
+     *
+     * @param CsrfTokenManagerInterface $csrfTokenManager
+     */
+    public function setCsrfTokenManager(CsrfTokenManagerInterface $csrfTokenManager): void
+    {
+        $this->csrfTokenManager = $csrfTokenManager;
+    }
+}
---
+++ src/Aware/Traits/UrlGeneratorAwareTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Bacart package.
+ *
+ * (c) Alex Bacart <alex@bacart.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bacart\SymfonyCommon\Aware\Traits;
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+trait UrlGeneratorAwareTrait
+{
+    /** @var UrlGeneratorInterface */
+    protected $urlGenerator;
+
+    /**
+     * @required
+     *
+     * @param UrlGeneratorInterface $urlGenerator
+     */
+    public function setUrlGenerator(UrlGeneratorInterface $urlGenerator): void
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+}
